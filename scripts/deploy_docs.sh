3060e148293681186b2c441c0d0a5f353451ab28
Dustin Firebaugh <dafirebaugh@gmail.com>
2024-01-18 21:05:49 -0500

    add deploy docs script

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+bbook build
+
+GIT_REPO_URL=$(git config --get remote.origin.url)
+
+cd .book
+
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "Static site deploy"
+git push github gh-pages --force
+
+cd ..

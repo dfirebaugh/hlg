238d524968d935996a1e35359e728aa09c0ec0bb
Dustin Firebaugh <dafirebaugh@gmail.com>
2024-08-28 18:56:55 -0400

    removing unused shader files

--- graphics/webgpu/internal/shader/shaders.go
+++ graphics/webgpu/internal/shader/shaders.go
@@ -13,37 +13,11 @@ var (
 	//go:embed texture.wgsl
 	textureShaderCode string
 
-	//go:embed line.wgsl
-	lineShaderCode string
-
-	//go:embed sdf_circle_outline.wgsl
-	circleOutlineShaderCode string
-
-	//go:embed sdf_circle_fill.wgsl
-	circleFillShaderCode string
-
-	//go:embed sdf_rect_outline.wgsl
-	rectOutlineShaderCode string
-
-	//go:embed sdf_rect_fill.wgsl
-	rectFillShaderCode string
-
-	ShapeShader         graphics.ShaderHandle
-	TextureShader       graphics.ShaderHandle
-	LineShader          graphics.ShaderHandle
-	CircleOutlineShader graphics.ShaderHandle
-	CircleFillShader    graphics.ShaderHandle
-	RectOutlineShader   graphics.ShaderHandle
-	RectFillShader      graphics.ShaderHandle
+	ShapeShader   graphics.ShaderHandle
+	TextureShader graphics.ShaderHandle
 )
 
 func CompileShaders(sm *ShaderManager) {
 	ShapeShader = sm.CompileShader(shapeShaderCode)
 	TextureShader = sm.CompileShader(textureShaderCode)
-
-	// LineShader = sm.CompileShader(lineShaderCode)
-	// CircleOutlineShader = sm.CompileShader(circleOutlineShaderCode)
-	// CircleFillShader = sm.CompileShader(circleFillShaderCode)
-	// RectOutlineShader = sm.CompileShader(rectOutlineShaderCode)
-	// RectFillShader = sm.CompileShader(rectFillShaderCode)
 }
